343f8d75193079f43fcfb2e910a0c60cf095fccd
NTGAYEON-PC\NTGAYEON <yyangel05@gmail.com>
2018-06-16 10:29:15 +0900

    180616 윤가연 마이페이지 사용자 대여목록 기능수정, 그외 간단한 버그 수정

--- src/my/MyReservationListAction.java
+++ src/my/MyReservationListAction.java
@@ -12,11 +12,7 @@ import org.apache.struts2.interceptor.SessionAware;
 import java.io.Reader;
 import java.io.IOException;
 
-import admin.pagingAction2;
-import member.MemberVO;
-import toy.toyReserveVO;
 import my.MyRentVO;
-
 
 import member.MemberVO;
 import org.apache.struts2.interceptor.SessionAware;
@@ -25,9 +21,6 @@ import java.sql.Timestamp;
 public class MyReservationListAction extends ActionSupport implements SessionAware {
 	
 	public static Reader reader;
-	
-
-
 	public static SqlMapClient sqlMapper;
 	
 	private  MyRentVO mparamClass = new MyRentVO();
@@ -36,18 +29,7 @@ public class MyReservationListAction extends ActionSupport implements SessionAwa
 	private String member_id;
 	private Map session;
 	
-	
-	
-
-
-
-/*	private int currentPage = 1; //현재 페이지
-*/	private int totalCount; 	//총 게시물의 수
-/*	private int blockCount = 5; //한 페이지의 게시물의 수
-	private int blockPage = 5; //한 화면에 보여줄 페이지 수
-	private String pagingHtml; //페이징을 구현한 html
-	private pagingAction2 page; //페이징 클래스
-*/
+	private int totalCount; 	//총 게시물의 수
 
 	private List<MyRentVO> MyReservationlist = new ArrayList<MyRentVO>();
 	
@@ -70,28 +52,6 @@ public class MyReservationListAction extends ActionSupport implements SessionAwa
 					
 				
 		totalCount = MyReservationlist.size(); //전체 글 갯수를 구함
-				
-
-		
-		//pagingAction 객체 생성
-/*		page = new pagingAction2(currentPage, totalCount, blockCount, blockPage);
-		
-		pagingHtml = page.getPagingHtml().toString(); //페이지 html 생성
-		
-		//현재 페이지에서 보여 줄 마지막 글의 번호 설정
-		int lastCount = totalCount;
-		
-		// 현재 페이지의 마지막 글의 번호가 전체의 마지막 글 번호보다 작으면 
-		//lastCount를 +1 번호로 설정.
-		if(page.getEndCount() < totalCount) {
-			lastCount = page.getEndCount() +1;
-		}
-		
-		//전체 리스트에서 현재 페이지만큼의 리스트만 가져온다
-		RentInfolist = RentInfolist.subList(page.getStartCount(), lastCount);
-		zizumlist = zizumlist.subList(page.getStartCount(), lastCount);
-				
-		*/
 	
 		return SUCCESS;
 	}
@@ -145,6 +105,7 @@ public class MyReservationListAction extends ActionSupport implements SessionAwa
 	public void setMember_id(String member_id) {
 		this.member_id = member_id;
 	}
+	
 	public Map getSession() {
 		return session;
 	}
@@ -153,10 +114,5 @@ public class MyReservationListAction extends ActionSupport implements SessionAwa
 	public void setSession(Map session) {
 		this.session = session;
 	}
-
-
-
-
-
 
 }
